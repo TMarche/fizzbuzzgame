d94266e3b653127c4fa9efb72c72acec3aa613c3
Troy Marchesseault <tmarchess@gmail.com>
2022-08-16 16:07:48 -0400

    Make initial changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    let [counter, setCounter] = useState(0);
+
+    return (
+        <div>
+            <div className="counter">
+                {counter}
+                <button
+                    className="counter__button"
+                    onClick={() => setCounter(counter + 1)}
+                >
+                    Increment
+                </button>
+            </div>
+            <div className="form">
+                <input placeholder="Enter something..."></input>
+            </div>
+        </div>
+    );
+};
 
 export default App;
