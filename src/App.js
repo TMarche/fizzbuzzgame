a50c03ce86489f399e7419e01d00c87981f1c64d
Troy Marchesseault <tmarchess@gmail.com>
2022-08-16 17:37:49 -0400

    Add basic functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,94 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 const App = () => {
-    let [counter, setCounter] = useState(0);
+    // let [counter, setCounter] = useState(0);
+    // let [inputValue, setInputValue] = useState("Pedro");
+
+    // Computes a new value between min/max inclusive
+    const generateValue = (min, max) => {
+        return Math.floor(Math.random() * (max - min) + min);
+    };
+
+    let [value, setValue] = useState(generateValue(1, 100));
+    let [grades, setGrades] = useState([]);
+
+    useEffect(() => {
+        document.addEventListener("keydown", detectKeyDown, true);
+    }, []);
+
+    const detectKeyDown = (e) => {
+        console.log("Clicked Key: ", e.key);
+    };
+
+    const validateFactory = (predicate) => {
+        return () => {
+            if (predicate()) {
+                setGrades([...grades, "P"]);
+            } else {
+                setGrades([...grades, "F"]);
+            }
+        };
+    };
+
+    const validateFizz = validateFactory(
+        () => value % 3 === 0 && value % 5 !== 0
+    );
+    const validateBuzz = validateFactory(
+        () => value % 3 !== 0 && value % 5 === 0
+    );
+    const validateFizzBuzz = validateFactory(
+        () => value % 3 === 0 && value % 5 === 0
+    );
+    const validateValue = validateFactory(
+        () => value % 3 !== 0 && value % 5 !== 0
+    );
 
     return (
-        <div>
-            <div className="counter">
-                {counter}
+        <div class="content">
+            {/* Display a random value from 1 to 100*/}
+            <div className="rules">Fizz = 3, Buzz = 5</div>
+            <div>
                 <button
-                    className="counter__button"
-                    onClick={() => setCounter(counter + 1)}
+                    className="button button-fizz"
+                    onClick={() => {
+                        validateFizz();
+                        setValue(generateValue(1, 100));
+                    }}
                 >
-                    Increment
+                    {"⬆\nFizz"}
                 </button>
-            </div>
-            <div className="form">
-                <input placeholder="Enter something..."></input>
+                <div className="center-row">
+                    <button
+                        className="button button-value"
+                        onClick={() => {
+                            validateValue();
+                            setValue(generateValue(1, 100));
+                        }}
+                    >
+                        {`⬅ ${value}`}
+                    </button>
+                    <div className="value">{value}</div>
+                    <button
+                        className="button button-fizzbuzz"
+                        onClick={() => {
+                            validateFizzBuzz();
+                            setValue(generateValue(1, 100));
+                        }}
+                    >
+                        {"FizzBuzz ➡"}
+                    </button>
+                </div>
+                <button
+                    className="button button-buzz"
+                    onClick={() => {
+                        validateBuzz();
+                        setValue(generateValue(1, 100));
+                    }}
+                >
+                    {"Buzz\n⬇"}
+                </button>
+                <div className="grade">{grades.join(" ")}</div>
             </div>
         </div>
     );
